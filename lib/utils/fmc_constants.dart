4243f1880e010b7276141761d48a32f43e224965
findmycubeapp <43104748+findmycubeapp@users.noreply.github.com>
2018-09-28 22:15:38 -0500

    Removed APIKEY

--- lib/utils/fmc_constants.dart
+++ lib/utils/fmc_constants.dart
@@ -1,5 +1,5 @@
 class FMCConstants {
   static const String appName = "Find My Cube";
   static const String caption = "Space to Read";
-  static const String apikey = "AIzaSyBXJD8_NOX1-za62LUViNzhBUnd3Y9BGf0";
+  static const String apikey = "XXXXXXXX";
 }
